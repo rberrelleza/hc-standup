f4e2dccf0ee932accdab41df384fef94ed7075ff
Don Brown <mrdon@twdata.org>
2014-04-30 21:43:14 -0600

    Better intro msg

--- app.py
+++ app.py
@@ -21,7 +21,8 @@ def init():
     def _send_welcome(event):
         client = event['client']
         yield from client.send_notification(app.addon,
-            text="HC Standup was added to this room. Type '/standup I did (allthethings)' to get started.")
+            text="HC Standup was added to this room. Type '/standup I did *this*' to get started (yes, "
+                 "you can use Markdown).")
 
     app.addon.register_event('install', _send_welcome)
 
